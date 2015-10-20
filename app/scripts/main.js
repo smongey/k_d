b474f6dad1977b4c4907a9122d8d39359c0ff372
smongey <sean@aokstudio.com>
2015-10-20 20:02:15 +0100

    ajax, loader, etc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,10 @@ var kd = {
 	$w : $(window),
 	$d : $(document),
 	$b : $('body'),
-	url : window.location.href
+	url : window.location.href,
+	home : 'http://' + window.location.host + '/index.html',
+	slides : [],
+	currIndex : 0
 };
 
 
@@ -20,66 +23,33 @@ kd.$d.keypress(function (e) {
 
 kd.$d.on('click', 'a.about', function(e){
 
-	e.preventDefault();
-	l('about clicked');
-	// $('section.home').toggleClass('out');
-	$('a.about').addClass('active');
-	$('a.contact').removeClass('active');
-	window.history.pushState('', 'About &mdash; Kr&aring;kvik &amp; D&rsquo;Orazio', 'about.html');
-	$('section.about, section.contact, section.home').addClass('out');
-	setTimeout(function(){
-		$('#wrap').empty().load('about.html #wrap > *', function(){
-			$('body').removeClass().addClass('about');
-			setTimeout(function(){
-				$('section.about').removeClass('out');
-			}, 500)
-		});
-	}, 500);
-
+	kd.ajaxReq(e, 'about')
 
 }).on('click', 'a.contact', function(e){
 
-	e.preventDefault();
-	l('contact clicked');
-	// $('section.home').toggleClass('out');
-	$('a.contact').addClass('active');
-	$('a.about').removeClass('active');
-	window.history.pushState('', 'Contact &mdash; Kr&aring;kvik &amp; D&rsquo;Orazio', 'contact.html');
-	$('section.about, section.contact, section.home').addClass('out');
-	setTimeout(function(){
-		$('#wrap').empty().load('contact.html #wrap > *', function(){
-			$('body').removeClass().addClass('contact');
-			setTimeout(function(){
-				$('section.contact').removeClass('out');
-			}, 500)
-		});
-	}, 500);
-
+	kd.ajaxReq(e, 'contact');
 
 }).on('click', 'a.logo', function(e){
 
-	e.preventDefault();
-	l('home clicked');
-	window.history.pushState('', 'Kr&aring;kvik &amp; D&rsquo;Orazio', kd.url);
-	// $('section.home').toggleClass('out');
-	$('a.about, a.contact').removeClass('active');
-	$('section.about, section.contact, section.home').addClass('out');
-	setTimeout(function(){
-		$('#wrap').empty().load(kd.url + ' #wrap > *', function(){
-			$('body').removeClass().addClass('home');
-			setTimeout(function(){
-				$('section.home').removeClass('out');
-			}, 500)
-		});
-	}, 500);
+	kd.ajaxReq(e, 'home');
 
 }).on('click', 'a.totop', function(e){
+
 	e.preventDefault();
 
 	$('html, body').animate({
 		scrollTop: '0'
 	}, 1000, 'easeInOutQuint');
 
+}).on('click', '.home .third, .home .half', function(e){
+	l($(this).find('img').attr('src').length);
+});
+
+
+kd.$w.load(function(){
+	setTimeout(function(){
+		$('#loader').addClass('out');
+	}, 500);
 });
 
 
@@ -89,31 +59,52 @@ kd.$d.ready(function(){
 
 		$('body').addClass('contact');
 		$('a.contact').addClass('active');
+		$('section.contact').removeClass('out');
 
 	} else if (url.indexOf('about') > -1) {
 
 		$('body').addClass('about');
 		$('a.about').addClass('active');
+		$('section.about').removeClass('out');
 
 	} else {
+
 		$('section.home').removeClass('out');
+
+		kd.makeSlider();
+
+
 	}
 	l(kd.url);
-});
 
+	$('#loader .logo').removeClass('out')
+});
 
 // var popped = ('state' in window.history && window.history.state !== null),
 // 	initialURL = location.href;
 
 $(window).bind('popstate', function(e) {
 	e.preventDefault();
+	// l(location.href.indexOf('index'));
+	if(location.href.indexOf('index') > 0) {
+		// l('home');
+		kd.ajaxReq(e, 'home');
+	} else if (location.href.indexOf('contact') > 0) {
+		// l('contact');
+		kd.ajaxReq(e, 'contact');
+	} else if (location.href.indexOf('about') > 0) {
+		// l('about');
+		kd.ajaxReq(e, 'about');
+	}
+
+
 	// var lastlocation
   // Ignore inital popstate that some browsers fire on page load
   // var initialPop = !popped && location.href == initialURL
   // popped = true
   // if (initialPop) return;
-  //l(window.history)
-  //l(e);
+  // l(window.history)
+  // l(e);
 
 
   // showMailOverview(); // exmaple function to display all email since the user has click Back.
@@ -122,3 +113,84 @@ $(window).bind('popstate', function(e) {
 
 
 
+kd.ajaxReq = function(e){
+
+	switch(arguments[1]) {
+		case "home":
+			
+			e.preventDefault();
+			if (e.type === 'click') { 
+				window.history.pushState('', 'Kr&aring;kvik &amp; D&rsquo;Orazio', kd.home);
+				l('h fired');
+			}
+			$('a.about, a.contact').removeClass('active');
+			$('section.about, section.contact, section.home').addClass('out');
+			setTimeout(function(){
+				$('#wrap').empty().load(kd.home + ' #wrap > *', function(){
+					$('body').removeClass().addClass('home');
+					setTimeout(function(){
+						$('section.home').removeClass('out');
+					}, 500)
+				});
+			}, 500);
+
+			if (kd.slides === []){
+				l('arse');
+			}
+
+			break;
+
+		case "contact":
+			
+			e.preventDefault();
+			$('a.contact').addClass('active');
+			$('a.about').removeClass('active');
+			if (e.type === 'click') { 
+				window.history.pushState('', 'Contact &mdash; Kr&aring;kvik &amp; D&rsquo;Orazio', 'contact.html');
+				l('c fired');
+			}
+			$('section.about, section.contact, section.home').addClass('out');
+			setTimeout(function(){
+				$('#wrap').empty().load('contact.html #wrap > *', function(){
+					$('body').removeClass().addClass('contact');
+					setTimeout(function(){
+						$('section.contact').removeClass('out');
+					}, 500)
+				});
+			}, 500);
+			break;
+
+		case "about":
+			
+			e.preventDefault();
+			
+			$('a.about').addClass('active');
+			$('a.contact').removeClass('active');
+			
+			if (e.type === 'click') { 
+				window.history.pushState('', 'About &mdash; Kr&aring;kvik &amp; D&rsquo;Orazio', 'about.html');
+				l('a fired');
+			}
+			$('section.about, section.contact, section.home').addClass('out');
+			setTimeout(function(){
+				$('#wrap').empty().load('about.html #wrap > *', function(){
+					$('body').removeClass().addClass('about');
+					setTimeout(function(){
+						$('section.about').removeClass('out');
+					}, 500)
+				});
+			}, 500);
+			break;
+		default:
+			l('no param passed into ajax Request');
+	}
+}
+
+kd.makeSlider = function(){
+
+	$('.home img').each(function(i){
+		l($(this).attr('src'));
+		kd.slides.push($(this).attr('src'));
+	});
+
+}
